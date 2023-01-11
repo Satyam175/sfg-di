c4998002fad3e2c57f80e711d10a0c2d20501441
Satyam Kaushik <kaushiksatyam54@gmail.com>
2023-01-11 16:55:23 +0530

    Primary Beans & Qualifiers

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Component;
+
+
+public interface GreetingService {
+    String sayGreeting();
+}
---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+    SetterInjectedController setterInjectedController;
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new ConstructorGreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(setterInjectedController.getGreeting());
+    }
+}
